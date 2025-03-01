65a450f9ade904f43a2d2dd2cba59d10fa279853
Dhanushgowdamv <dhanushvenu199@gmail.com>
2025-03-01 21:12:50 +0530

    about section and animated the title is over

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Hero from './component/hero'
+import About from './component/about'
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
-      <section className='z-0 min-h-screen'/>
+      <About/>
     </main>
   )
 }
